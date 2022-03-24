83989c3a1a8201887ce607bfe376d5be031eaa53
Ayushree-2147141 <ayushree.chakrabartty@mca.christuniversity.in>
2022-03-24 10:56:06 +0530

    updated

--- lab4demo.c
+++ lab4demo.c
@@ -1,120 +1,146 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-
-struct Mobile
-{
-	char model[100];
-	char brand[50];
-	int mem;
-	float price;
-	struct Mobile* next;
+#define MAX 25
+/********************************************************************
+* STRUCTURE MOBILE *
+********************************************************************/
+struct mobile {
+   char model[MAX];
+   char brand[MAX];
+   int capacity;
+   float price;
 };
 
-struct Mobile* head;
-
-
-void insert()   
-{  
-    struct Mobile *mobile, *temp;
-	int count = 0, choice = 1;
-        
-    mobile = (struct Mobile*)malloc(sizeof(struct Mobile));  
-    if(mobile == NULL)  
-    {  
-        printf("\nOVERFLOW");     
-    }  
-    else  
-    {  
-       while(count < 5)
-	{
-		printf("\nEnter the details\n");
-		printf("1. Model : ");
-		scanf("%s",&mobile->model);
-		
-		printf("2. Brand : ");
-		scanf("%s",&mobile->brand);
-		
-		printf("3. Memory : ");
-		scanf("%d",&mobile->mem);
-		
-		printf("4. Price : ");
-		scanf("%f",&mobile->price);
-		
-		count = count + 1;
-		if(head == NULL)  
-        {  
-            mobile -> next = NULL;  
-            head = mobile;  
-            printf("\nNode inserted\n");  
-        }  
-        else  
-        {  
-            temp = head;  
-            while (temp -> next != NULL)  
-            {  
-                temp = temp -> next;  
-            }  
-            temp->next = mobile;  
-            mobile->next = NULL;  
-            printf("\nNode inserted\n");  
-          
-        }   
-	}    
-        
-    }  
-}  
-
-void display()
-{
-	struct Mobile *mobile;  
-    mobile = head;   
-    if(mobile == NULL)  
-    {  
-        printf("Nothing to print");  
-    }  
-    else  
-    {  
-        printf("\nPrinting values . . . . .\n");   
-        while (mobile!=NULL)  
-        {  
-            printf("\nModel : %s",mobile->model);
-			printf("\nBrand : %s",mobile->brand);
-			printf("\nMemory : %d",mobile->mem);
-			printf("\nPrice : %f",mobile->price);
-			
-            mobile = mobile -> next;  
-        }  
-    }  
-    printf("-------------------------------------------\n");
-}
+void display(struct mobile *mob, int size);
+void sort_brand(struct mobile *mob, int size);
+void sort_price(struct mobile *mob, int size);
 
 int main()
 {
-	int choice = 0;
-	printf("\nPress:\n1. Insert\n2. Exit\n3. Display\n");
-	scanf("%d",&choice);
+	int i, n, capacity;
+	char mob_model[MAX], mob_brand[MAX];
+	float price;
 	
-	while(choice != 2)
+	printf("Enter number of mobiles:");
+	scanf("%d", &n);
+	
+	struct mobile mob[n];
+	
+	for (i = 0; i < n; i++) 
 	{
-		switch(choice)
+		printf("\n");
+
+		printf("\nEnter mobile %d details :", i);
+		
+		printf("\nEnter mobile model : ");
+		scanf("%s", &mob[i].model);
+		
+		printf("\nEnter mobile brand : ");
+		scanf("%s", &mob[i].brand);
+		
+		printf("\nEnter mobile capacity : ");
+		scanf("%d", &mob[i].capacity);
+		
+		printf("\nEnter Selling price : ");
+		scanf("%f", &mob[i].price);
+		
+		printf("\n");
+	}
+		
+	sort_brand(mob, n);
+	sort_price(mob, n);
+   
+    return 0;
+}
+
+void display(struct mobile *mob, int size)
+{
+	int i;
+	for (i = 0; i < size; i++) 
+	{
+		printf("\n");
+		
+		printf("\n\t\t\tMobile %d details :", i);
+		
+		printf("\n\n\t\t\tMobile model : %s", mob[i].model);
+		
+		printf("\n\n\t\t\tMobile brand : %s", mob[i].brand);
+		
+		printf("\n\n\t\t\tMobile capacity : %d", mob[i].capacity);
+		
+		printf("\n\n\t\t\tMobile price : %2f", mob[i].price);
+		
+		printf("\n");
+   }
+}
+
+void sort_brand(struct mobile *mob, int size)
+{
+	int i, j, min_idx;
+	
+	char minStr[MAX];
+	for (i = 0; i < size-1; i++)
+	{
+		// Find the minimum element in unsorted array
+		int min_idx = i;
+		strcpy(minStr, mob[i].brand);
+		for (j = i+1; j < size; j++)
 		{
-			case 1:
-				insert();
-				break;
-			
-			case 2:
-				exit(0);
-				break;
-			
-			default:
-				printf("\nWrong choice!");
-				break;
-				
-			case 4:
-				display();
-				break;
+		    // If min is greater than arr[j]
+		    if (strcmp(minStr, mob[j].brand) > 0)
+		    {
+		        // Make arr[j] as minStr and update min_idx
+		        strcpy(minStr, mob[j].brand);
+		        min_idx = j;
+		    }
+		}
+		
+		// Swap the found minimum element with the first element
+		if (min_idx != i)
+		{
+		    struct mobile temp;
+		    temp = mob[i];
+		    mob[i] = mob[min_idx];
+		    mob[min_idx] = temp;
 		}
 	}
+	
+	display(mob, size);
+}
+
+void sort_price(struct mobile *mob, int size)
+{
+	int i, j, min_idx;
+	float min_price;
+		
+	for (i = 0; i < size-1; i++)
+	{
+		// Find the minimum element in unsorted array
+		int min_idx = i;
+		min_price = mob[i].price;
+		for (j = i+1; j < size; j++)
+		{
+		    // If min is greater than arr[j]
+		    if (min_price > mob[j].price)
+		    {
+		        // Make arr[j] as minStr and update min_idx
+		        min_price = mob[j].price;
+		        min_idx = j;
+		    }
+		}
+		
+		// Swap the found minimum element with the first element
+		if (min_idx != i)
+		{
+		    struct mobile temp;
+		    temp = mob[i];
+		    mob[i] = mob[min_idx];
+		    mob[min_idx] = temp;
+		}
+	}
+	
+	display(mob, size);
 }
 
